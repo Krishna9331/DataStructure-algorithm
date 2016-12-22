7761064e46596a625c251fbdcb4cc7228b0dd7f4
Krishna Kant Mishra <kkant551@gmail.com>
2016-12-22 15:09:53 +0530

    Problems on sorted list and Stack

---
+++ src/generalproblem/InsertInSortedDLinkList.java
@@ -0,0 +1,61 @@
+package generalproblem;
+
+import java.util.Stack;
+
+/**
+ * Created by mishrk3 on 12/21/2016.
+ */
+public class InsertInSortedDLinkList {
+	static class Node {
+		int data;
+		Node prev;
+		Node next;
+	}
+
+	Node SortedInsert(Node head,int data) {
+		Node newNode = new Node();
+		newNode.data = data;
+		Node p = head, q = null;
+		if (head == null) {
+			head = newNode;
+		} else {
+			while (p.next != null && p.data <= data) {
+				q = p;
+				p = p.next;
+			}
+			 if (p.data < newNode.data) {
+				p.next = newNode;
+				newNode.prev = p;
+				newNode.next = null;
+			}else if (q == null) {
+				newNode.next = head;
+				head.prev = newNode;
+				head = newNode;
+			} else {
+				q.next = newNode;
+				newNode.prev = q;
+				newNode.next = p;
+				p.prev = newNode;
+			}
+		}
+		return head;
+	}
+
+
+	public static void main(String[] args) {
+		Node node = new Node();
+		node.data = 8;
+		InsertInSortedDLinkList in = new InsertInSortedDLinkList();
+
+		Node head = in.SortedInsert(null, 1);
+		Node head1 = in.SortedInsert(head, 4);
+		Node head2 = in.SortedInsert(head1, 2);
+		Node head3 = in.SortedInsert(head2, 3);
+		Node head4 = in.SortedInsert(head3, 7);
+		Node head5 = in.SortedInsert(head4, 6);
+		Node head6 = in.SortedInsert(head5, 9);
+		Node head7 = in.SortedInsert(head6, 10);
+		System.out.println("All insert done: " + head7);
+	}
+
+}
---
+++ src/generalproblem/MaxElementInStack.java
@@ -0,0 +1,70 @@
+package generalproblem;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Created by mishrk3 on 12/21/2016.
+ *
+ * You have an empty sequence, and you will be given  queries. Each query is one of these three types:
+
+ 1 x  -Push the element x into the stack.
+ 2    -Delete the element present at the top of the stack.
+ 3    -Print the maximum element in the stack.
+ Input Format
+
+ The first line of input contains an integer, . The next  lines each contain an above mentioned query. (It is guaranteed that each query is valid.)
+
+ Constraints
+
+
+
+ Output Format
+
+ For each type  query, print the maximum element in the stack on a new line.
+
+ Sample Input
+
+ 10
+ 1 97
+ 2
+ 1 20
+ 2
+ 1 26
+ 1 20
+ 2
+ 3
+ 1 91
+ 3
+ Sample Output
+
+ 26
+ 91
+ */
+public class MaxElementInStack {
+	public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+		Stack<Integer> sc = new Stack();
+		Stack<Integer> sc1 = new Stack();
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		for(int i = 0; i < n; i++){
+			int cmd = scn.nextInt();
+
+			if(cmd == 1){
+				int data = scn.nextInt();
+				sc.push(data);
+				if(sc1.empty() || sc1.peek() <= data){
+					sc1.push(data);
+				}
+			}else if(cmd == 2){
+				int d = sc.pop();
+				if(sc1.peek() == d){
+					sc1.pop();
+				}
+			}else{
+				System.out.println(sc1.peek());
+			}
+		}
+	}
+}
