c73dd077ad56264605533cee824fe428828fccc3
Krishna Kant Mishra <kkant551@gmail.com>
2017-03-13 11:15:15 +0530

    Drawback of counting sort

--- src/chapter10/CountingSort.java
+++ src/chapter10/CountingSort.java
@@ -7,9 +7,15 @@ import java.util.Arrays;
  */
 
 /**
+ * <pre>
  * Counting sort is not comparision based sorting, It is applied on an array containing elements in given range. it
  * counts number of smaller element in array for a given key, and that's how it decides the position of key. e.g: if 10
  * elements are less than a key X in array this means position of X in array is 11th.
+ * 
+ * The major draw back of counting sort is it requires temporary space of size equal to 1 + maximum element in array.
+ * If you look at second example in main method total number of element in array is 10 however, counting sort require
+ * space of 671 as maximum element in array is 670.
+ * </pre>
  */
 public class CountingSort {
 
@@ -50,9 +56,21 @@ public class CountingSort {
 		int[] numbers = {
 				1, 5, 7, 6, 5, 2, 3, 8, 7
 		};
-
-		cs.sort(numbers, 9);
-		System.out.println("Array is sorted.......");
+		System.out.print("Actual Array: ");
 		Arrays.stream(numbers).forEach(num -> System.out.print(num + " "));
+		System.out.println();
+		cs.sort(numbers, 9);
+		System.out.print("Array after sorting: ");
+		Arrays.stream(numbers).forEach(num -> System.out.print(num + " "));
+		System.out.println();
+		int[] num = {
+				126, 348, 343, 432, 316, 171, 556, 223, 670, 201
+		};
+		System.out.print("Actual Array: ");
+		Arrays.stream(num).forEach(n -> System.out.print(n + " "));
+		System.out.println();
+		cs.sort(num, 671);
+		System.out.print("Array after sorting: ");
+		Arrays.stream(num).forEach(n -> System.out.print(n + " "));
 	}
 }
