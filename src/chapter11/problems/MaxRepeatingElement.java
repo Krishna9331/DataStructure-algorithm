d1a2c44462674fa3c6d5d1560b800e65cfbf6119
Krishna Kant Mishra <kkant551@gmail.com>
2017-03-29 21:34:50 +0530

    Searching  Finding max repeating element in array   -The array contains 1 to n-1 elements where n is length of array   -Array is not read only

---
+++ src/chapter11/problems/MaxRepeatingElement.java
@@ -0,0 +1,56 @@
+package chapter11.problems;
+
+/**
+ * Created by mishrk3 on 3/29/2017.
+ */
+
+/**
+ * Given an array where all elements of array ranges from 0 to n-1, write an algorithm to find the element which has
+ * maximum number of repetition.
+ */
+public class MaxRepeatingElement {
+
+	/**
+	 * @param elements
+	 *            the array of integer
+	 * @return the element having maximum number of repetition<br/>
+	 *         <pre>
+	 *     Method works on the below logic
+	 *      Add length of the array to each index represented by the elements in array means
+	 *       {3, 2, 1, 2, 2, 3}
+	 *       the first element 3 will add array length 6 in the element at index 3, which will make array as below:
+	 *        {3, 2, 1, 8, 2, 3}
+	 *        So if we analyse, the element which repeated most of the times will add 6 that many times to the index.
+	 *        Hence for the above array we will add 6, 3 times to the element at 2nd position bacause 2 is repeated 3
+	 *        times.
+	 *        Also for evaluating index we always do modulo by 6, so that it will always produce the original number
+	 *        of array, e.g if we consider {3, 2, 1, 8, 2, 3}, the 8 is not the actual array element it got modified
+	 *        after processing the first element of array.
+	 * </pre>
+	 */
+	public int findMaxRepeatingElement(int[] elements) {
+		int max = 0;
+		int maxIndex = 0;
+		int n = elements.length;
+		for (int i = 0; i < n; i++) {
+			elements[elements[i] % n] += n;
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (elements[i] / n > max) {
+				max = elements[i] / n;
+				maxIndex = i;
+			}
+		}
+		return maxIndex;
+	}
+
+	public static void main(String[] args) {
+
+		MaxRepeatingElement mre = new MaxRepeatingElement();
+		int[] elements = {
+				3, 2, 1, 2, 2, 3
+		};
+		System.out.println("Max repeating element is: " + mre.findMaxRepeatingElement(elements));
+	}
+}
