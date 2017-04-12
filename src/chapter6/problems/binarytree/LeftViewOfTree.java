e3c74cdf38c92d3c9740ce57b4c777843800e3c4
Krishna Mishra <kkant551@gmail.com>
2017-04-12 19:26:31 +0530

    Left view of tree

---
+++ src/chapter6/problems/binarytree/LeftViewOfTree.java
@@ -0,0 +1,57 @@
+package chapter6.problems.binarytree;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import chapter6.problems.binarytree.SampleBinaryTree.Node;
+
+/**
+ * Created by mishrk3 on 4/12/2017.
+ */
+public class LeftViewOfTree {
+
+	private class QItem {
+		Node<Integer> item;
+		int level;
+
+		public QItem(Node<Integer> item, int level) {
+			this.item = item;
+			this.level = level;
+		}
+	}
+
+	public void leftView(Node<Integer> root) {
+		int maxLevel = -1;
+		QItem qi = new QItem(root, 0);
+		Queue<QItem> q = new ArrayDeque<>();
+		q.add(qi);
+
+		while (!q.isEmpty()) {
+			qi = q.remove();
+			Node<Integer> temp = qi.item;
+			int hd = qi.level;
+			if (hd > maxLevel) {
+				maxLevel = hd;
+				System.out.println(temp.item);
+			}
+
+			if (temp.left != null) {
+				qi = new QItem(temp.left, hd + 1);
+				q.add(qi);
+			}
+
+			if (temp.right != null) {
+				qi = new QItem(temp.right, hd + 1);
+				q.add(qi);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		LeftViewOfTree lvot = new LeftViewOfTree();
+		SampleBinaryTree<Integer> tree = SampleBinaryTree.getDefaultTree();
+		tree.getRoot().left.left.right = new Node<>(23, null, null);
+		tree.getRoot().left.left.right.left = new Node<>(43, null, null);
+		lvot.leftView(tree.getRoot());
+	}
+}
